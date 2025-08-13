8bb9b3fb7dc7743453c9e8e05b8f4be844c85fe5
AI-Codemaster <ai5445359@gmail.com>
2025-08-13 21:54:31 +0600

    Complete Problem 01

---
+++ problem_01.js
@@ -0,0 +1,10 @@
+function totalFine(fare) {
+  if (typeof fare !== 'number' || fare <= 0) {
+    return 'Invalid';
+  }
+  const fine = (fare * 20) / 100;
+
+  const totalAmount = fare + fine + 30;
+
+  return totalAmount;
+}
